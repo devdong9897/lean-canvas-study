7a21c611e0a04a7854d03e935aad6e203d43c63a
devdong9897 <dev.dong9897@gmail.com>
2025-04-29 15:28:42 +0900

    feat: 레이아웃UI

--- src/pages/CanvasDetail.jsx
+++ src/pages/CanvasDetail.jsx
@@ -1,10 +1,12 @@
 // 캔버스 클릭시 들어가는 컴포넌트.
 import CanvasTitle from '../components/CanvasTitle';
+import LeanCanvas from '../components/LeanCanvas';
 
 function CanvasDetail() {
   return (
     <div>
       <CanvasTitle />
+      <LeanCanvas /> 
     </div>
   );
 }
